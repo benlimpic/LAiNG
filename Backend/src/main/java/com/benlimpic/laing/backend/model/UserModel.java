6441e718ec0f30d58864f4321552b0ccc66575db
init.defaultBranch <benlimpic@gmail.com>
2025-07-03 12:35:50 -0700

    basic relationships db

--- Backend/src/main/java/com/benlimpic/laing/backend/model/UserModel.java
+++ Backend/src/main/java/com/benlimpic/laing/backend/model/UserModel.java
@@ -3,6 +3,7 @@ package com.benlimpic.laing.backend.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import jakarta.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -10,11 +11,16 @@ import lombok.AllArgsConstructor;
 @Entity
 @Table(name = "users")
 public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
     private String email;
     private String firstName;
     private String lastName;
-    private String role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "story_id")
+    private ArrayList<StoryModel> stories;
 }
